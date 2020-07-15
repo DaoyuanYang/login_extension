1eb2fa192dd501cf2308e4d0a44ec17c2f0ddb05
Daoyuan Yang <u6547564@anu.edu.au>
2020-07-15 23:02:02 +1000

    clear to make repository public

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-// var webdriver = require("selenium-webdriver"),
-// SeleniumServer = require("selenium-webdriver/remote").SeleniumServer;
-// alert('Hi')
-
 var username = document.getElementById('username')
 var password = document.getElementById('password')
 var continue_but = document.getElementsByName('_eventId_proceed')[0]
 
-username.value = "u6547564"
-password.value = "dOMEY1999$"
-
-// function sleep(ms) {
-//     console.log('Start sleeping')
-//     return new Promise(resolve => setTimeout(resolve, ms));
-//   }
-
-// await sleep(2)
+username.value = 'cred_username'
+password.value = 'cred_password'
 
 continue_but.click()
 
